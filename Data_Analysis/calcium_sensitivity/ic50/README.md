0d18ddfb63948d8308e4e5b27f92c5cb03ab67cd
agrawal-aditi <aditi.agrawal@dtc.ox.ac.uk>
2021-12-15 15:00:06 +0000

    regression test

---
+++ Data_Analysis/calcium_sensitivity/ic50/README.md
@@ -0,0 +1,5 @@
+The assumptions of the linear regression line (after taking logarithm of both the volume and the IC50 values) are tested graphically in [Figure 12](https://github.com/CardiacModelling/ical-review/blob/master/Data_Analysis/calcium_sensitivity/ic50/figure12.pdf) this [figure](https://github.com/CardiacModelling/ical-review/blob/master/Data_Analysis/calcium_sensitivity/ic50/linear_regression_test.pdf).
+The assumptions of are discussed below:
+1. Homoscedasticity: Subplot 1 (left) shows that there the residuals are randomly distributed about the y = 0 line indicating that the variance is not dependent on the level of the predicted value.
+2. Normal distribution: Subplot 2 (centre) shows that the quantile-quantile plot falls roughly on a 45&deg; angle indicating a normal distribution of the residuals.
+3. Independence of datapoints: Subplot 1 (left) and subplot 3 (right) show that there is no discernible pattern of the residuals with respect to either the predicted value or the independent variable.
---
+++ Data_Analysis/calcium_sensitivity/ic50/regression_test.py
@@ -0,0 +1,64 @@
+import pandas as pd 
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
+import scipy.stats as stats
+
+# load data
+data = pd.read_csv('ic50.csv', usecols=['x', 'y'])
+data = data.sort_values('x')
+vol = data['x'].to_numpy()
+ic50 = data['y'].to_numpy()
+vol = np.log(vol)
+ic50 = np.log(ic50)
+vol_fit = vol.reshape((-1, 1))
+
+
+model = LinearRegression().fit(vol_fit, ic50)
+
+r_sq = model.score(vol_fit, ic50)
+c = model.intercept_
+m = model.coef_
+m = m[0]
+
+pred = m*vol + c
+res = ic50 - pred
+
+fig = plt.figure(figsize=(6.6, 3))
+ax1 = fig.add_subplot(131)
+
+### Does not make sense for non-time series data
+# res_data = pd.DataFrame(res)
+# pd.plotting.autocorrelation_plot(res_data)
+# plt.grid('off')
+# plt.show()
+
+# residuals: independent (non-random pattern around 0), 
+# residuals: homoscodasticity (variance does not depend on the magnitude of prediction)
+ax1.scatter(pred, res)
+ax1.axhline(y=0, color = 'red')
+ax1.set_xlabel('Predicted value of IC50')
+ax1.set_ylabel('Residual')
+
+
+
+# normality: 
+# qq plot should fall on 45 degree angle
+ax2 = fig.add_subplot(132)
+stats.probplot(res, dist="norm", plot=ax2)
+ax2.get_lines()[0].set_markerfacecolor('#1f77b4')
+ax2.get_lines()[0].set_markeredgecolor('#1f77b4')
+ax2.set_ylabel('')
+ax2.set_xlabel('Theoretical Quantile')
+ax2.set_title('')
+
+# independence wrt to the independent variabele vol
+ax3 = fig.add_subplot(133)
+ax3.scatter(vol, res)
+ax3.set_xlabel('Volume')
+
+plt.tight_layout()
+plt.savefig('linear_regression_test.pdf')
+plt.close()
+
