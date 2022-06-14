c92a4d0ddb62169e6075999fa0cf47aede135e6a
Aditi <aditi.agrawal@dtc.ox.ac.uk>
2022-06-15 00:34:03 +0100

    changes in the compensation figure

--- Data_Analysis/Kinetic_protocols/figure17.py
+++ Data_Analysis/Kinetic_protocols/figure17.py
@@ -17,12 +17,13 @@ models = ['priebe-1998.mmt', 'inada-2009.mmt']
 matplotlib.rc('font', family='arial', size = 8)
 
 fig = plt.figure(figsize = (6.8, 2.6))
-fig.subplots_adjust(0.085, 0.14, 0.99, 0.99, wspace=0.25)
-ax1 = fig.add_subplot(1, 2, 1)
-ax2 = fig.add_subplot(1, 2, 2)
+fig.subplots_adjust(0.085, 0.14, 0.99, 0.99, wspace=0.3)
+ax1 = fig.add_subplot(1, 3, 1)
+ax2 = fig.add_subplot(1, 3, 2)
+ax3 = fig.add_subplot(1, 3, 3)
 
-axes = [ax2]
-pro_step1 = [60.01]
+axes = [ax2, ax3]
+pro_step1 = [10.01, 60.01]
 
 for ax, step in zip(axes, pro_step1):
     for model in models:
@@ -62,6 +63,48 @@ for ax, step in zip(axes, pro_step1):
 
         print('-----------')
 
+# axes = [ax1]
+# pro_step1 = [60.01]
+
+# for ax, step in zip(axes, pro_step1):
+#     for model in models:
+
+#         print(model)
+#         m = myokit.load_model(model)
+
+#         v = m.get('membrane.V')
+#         v.set_binding(None)
+#         s = myokit.Simulation(m)
+
+#         # Stabilise the model a steady state by running for 10 seconds
+#         s.set_constant('membrane.V', - 90.01)
+#         s.run(10000, log = [])
+#         s.set_time(0)
+
+#         # Step to 60 mV
+#         s.set_constant('membrane.V', step)
+#         d = s.run(150, log=['L_type_Ca_current.i_CaL', 'environment.time'])
+
+#         tlast = d['environment.time'][-1]
+#         ical = d['L_type_Ca_current.i_CaL']
+#         ical_min = np.min(d['L_type_Ca_current.i_CaL'])
+
+#         # Step to 5 mV
+#         s.set_constant('membrane.V', 5.01)
+#         d = s.run(150, d).npview()
+
+#         ical = d['L_type_Ca_current.i_CaL']
+
+#         if model[0] == 'p':
+#             lab = 'Priebe & Beuckelmann 1998'
+#         else:
+#             lab = 'Inada et al. 2009'
+
+#         ax.plot(d['environment.time'], ical / -ical_min, label=lab)
+
+#         print('-----------')
+
+
 '''
 # Voltage protocol
 ax1.axhline(-1, xmin = 0.35, xmax = 0.4, color = 'black', ls = '-')
@@ -85,42 +128,64 @@ ax2.axhline(-2.3, xmin=0.6, xmax=0.65, color = 'black', ls = '-')
 
 '''
 
-ax2.plot(
+ax3.plot(
     [0.1, 0.2, 0.2, 0.3, 0.3, 0.4],
     [0.1, 0.1, 0.4, 0.4, 0.21, 0.21],
+    'k', transform=ax3.transAxes)
+
+ax3.text(2, -1.9, '$-90$ mV')
+ax3.text(42, -1.25, '$+60$ mV')
+ax3.text(90, -1.70, '$+5$ mV')
+
+ax2.plot(
+    [0.55, 0.65, 0.65, 0.75, 0.75, 0.85],
+    [0.1, 0.1, 0.3, 0.3, 0.21, 0.21],
     'k', transform=ax2.transAxes)
 
-ax2.text(-7, -2.05, '$-90$ mV')
-ax2.text(50, -1.32, '$+60$ mV')
-ax2.text(90, -1.80, '$+5$ mV')
+ax2.text(112, -0.92, '$-90$ mV')
+ax2.text(190, -0.7, '$+10$ mV')
+ax2.text(235, -0.80, '$+5$ mV')
 
 
 # Step transition
 ax2.axvline(tlast, ls = '--', color = 'grey')
+ax3.axvline(tlast, ls = '--', color = 'grey')
 
 ax2.set_xlabel('Time (ms)')
 
 ax2.set_ylabel('Current (normalised)')
 
+ax3.set_xlabel('Time (ms)')
+
+ax3.set_ylabel('Current (normalised)')
+
+ax2.tick_params(bottom = True, top = True, left = True, right = True)
+
 #ax2.legend(frameon=False)
+
+ax1.xaxis.set_minor_locator(MultipleLocator(10))
+ax1.yaxis.set_minor_locator(MultipleLocator(0.1))
 
 ax2.xaxis.set_minor_locator(MultipleLocator(10))
 ax2.yaxis.set_minor_locator(MultipleLocator(0.1))
 
+ax3.xaxis.set_minor_locator(MultipleLocator(10))
+ax3.yaxis.set_minor_locator(MultipleLocator(0.1))
+
 # Plot IV curves from web lab
 ax1.set_xlabel('Membrane potential (mV)')
-ax1.set_ylabel('Peak Current (normalised')
+ax1.set_ylabel('Peak Current (normalised)')
 ax1.set_xlim(-45, 60)
 ax1.set_ylim(-1.05, 0.15)
 d = myokit.DataLog.load_csv('data/iv-priebe-inada.csv')
-ax1.plot(d['vm'], d['priebe'], label='Priebe & Beuckelmann 1998')
-ax1.plot(d['vm'], d['inada'], label='Inada et al. 2009')
-ax1.legend(frameon=False, loc=(0.25, 0.81))
+ax1.plot(d['vm'], d['priebe'], label='Priebe 1998')
+ax1.plot(d['vm'], d['inada'], label='Inada 2009')
+ax1.legend(frameon=False, loc= 'upper right')
 
 
 
 
-#plt.tight_layout()
+# plt.tight_layout()
 plt.savefig('figure17.eps')
 plt.savefig('figure17.pdf')
 plt.close()
